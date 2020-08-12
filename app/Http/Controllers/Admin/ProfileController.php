c822d406b3dffa80b8a8d52cdbc5f5698e0c62a5
___mo <0.km.mo.tk@gmail.com>
2020-08-12 08:02:04 +0000

    No.26の課題

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -16,26 +16,46 @@ class ProfileController extends Controller
     
     public function create(Request $request)
     {
+        // Validationを行う
         $this->validate($request, Profile::$rules);
-        
         $profile = new Profile;
         $form = $request->all();
         
+        // フォームから送信されてきた_tokenを削除する
         unset($form['_token']);
         
+        // データベースに保存する
         $profile->fill($form);
         $profile->save();
         
         return redirect('admin/profile/create');
     }
     
-    public function edit()
+    public function edit(Request $request)
     {
-        return view('admin.profile.edit');
+        // Profile Modelからデータを取得する
+        $profile = Profile::find($request->id);
+        if (empty($profile)) {
+            abort(404);
+        }
+        
+        return view('admin.profile.edit', ['profile_form' => $profile]);
     }
     
-    public function update()
+    public function update(Request $request)
     {
-        return redirect('admin/profile/edit');
+        
+        // Validationをかける
+        $this->validate($request, Profile::$rules);
+        // Profile Modelからデータを取得する
+        $profile = Profile::find($request->id);
+        // 送信されたフォームデータを格納する
+        $profile_form = $request->all();
+        // フォームから送信されてきた_tokenを削除する
+        unset($profile_form['_token']);
+        // 該当するデータを上書きして保存する
+        $profile->fill($profile_form)->save();
+        
+        return redirect('admin/profile/create');
     }
 }
